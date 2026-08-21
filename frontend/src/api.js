8588b8e467ccdb531698ce818a040dc1dec83385
Dipesh-Raj-S <dipeshraj.suresh@gmail.com>
2026-08-22 00:57:15 +0530

    fixing axios api after deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -11,7 +11,7 @@ import axios from 'axios';
  * For this timeline, localStorage is an acceptable tradeoff.
  */
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL || '/api',
   headers: {
     'Content-Type': 'application/json',
   },
